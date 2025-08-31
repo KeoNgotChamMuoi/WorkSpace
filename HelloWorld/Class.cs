d96d0901fcd8aa6cda747120ca687b51c8154234
KEONGOTCUTIE <cvavietanh31@gmail.com>
2025-08-31 09:56:47 +0700

    first commit

---
+++ HelloWorld/Class.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    public class Class
+    {
+        public String ReturnMessage()
+        {
+            return "Happy coding!";
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Console.WriteLine("Hello World!");
+            var c1 = new Class();
+            Console.WriteLine($"HelloWorld! {c1.ReturnMessage()}");
+        }
+    }
+}
