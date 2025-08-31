1ec6220e2ad836473e8e54486392247ddd749ea7
KEONGOTCUTIE <cvavietanh31@gmail.com>
2025-08-31 10:10:28 +0700

    TH-sdToanTu

---
+++ sdToanTu/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sdToanTu
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            float width, height, area;
+            Console.WriteLine("Enter Width:");
+            width = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height:");
+            height = float.Parse(Console.ReadLine());
+            area = width * height;
+            Console.WriteLine("Area is:"+ area);
+        }
+    }
+}
