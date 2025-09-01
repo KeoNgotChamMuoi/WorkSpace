744722204d347dcbb188cefb2893c779c3bc9410
KEONGOTCUTIE <cvavietanh31@gmail.com>
2025-09-01 13:01:25 +0700

    BT_chuyen_doi_tien_te

---
+++ BT_Chuyen_doi_tien_te/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int rate = 23000;
+
+        Console.Write("Enter USD: ");
+        string? input = Console.ReadLine();
+
+        if (int.TryParse(input, out int usd))
+        {
+            int vnd = usd * rate;
+            Console.WriteLine($"{usd} USD = {vnd} VND");
+        }
+        else
+        {
+            Console.WriteLine("Giá trị nhập vào không hợp lệ!");
+        }
+    }
+}
