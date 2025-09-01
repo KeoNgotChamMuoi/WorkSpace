92c68c72f47720e12498a3d706a5c6683c0c0d9d
KEONGOTCUTIE <cvavietanh31@gmail.com>
2025-09-01 12:35:17 +0700

    hello2

---
+++ TH_hello2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TH_hello2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string yourName;
+            Console.WriteLine("Enter your name:");
+            yourName = Console.ReadLine();
+            Console.WriteLine("Hello "+ yourName );
+        }
+    }
+}
