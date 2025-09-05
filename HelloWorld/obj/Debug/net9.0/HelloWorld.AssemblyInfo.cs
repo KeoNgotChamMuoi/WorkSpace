6483e7bda8bd595329fd101b6e0a8697d0a0b127
KEONGOTCUTIE <cvavietanh31@gmail.com>
2025-09-05 14:52:57 +0700

    system

--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6af2b76e442262b2b255a2792891a84626ca553")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ea5764eaf0c29ad62a4d1e8cd75a977c0fec95")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
