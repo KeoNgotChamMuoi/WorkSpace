7daddee207f0f91c3fa5619cf2790bd0785885e7
KEONGOTCUTIE <cvavietanh31@gmail.com>
2025-10-11 21:12:13 +0700

    update test 11_10

---
+++ test1110/Bai1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Phone
+{
+    public string Name;
+    public int Price;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Price: {Price}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Phone[] phones = new Phone[3];
+        phones[0] = new Phone { Name = "iPhone", Price = 20000 };
+        phones[1] = new Phone { Name = "Samsung", Price = 15000 };
+        phones[2] = new Phone { Name = "Xiaomi", Price = 10000 };
+
+        Console.WriteLine("Danh sách điện thoại ban đầu:");
+        foreach (var i in phones)
+        {
+            i.ShowInfo();
+        }
+
+        Phone p1 = phones[0];
+        p1.Price = 9999;
+
+        Console.WriteLine("\nDanh sách điện thoại sau khi thay đổi giá của p1:");
+        foreach (var i in phones)
+        {
+            i.ShowInfo();
+        }
+
+        // Phone là reference type nên thay đổi p1 ảnh hưởng đến phones[0]
+      
+        List<Phone> phoneList = new List<Phone>
+        {
+            new Phone { Name = "Oppo", Price = 12000 },
+            new Phone { Name = "Vivo", Price = 13000 }
+        };
+
+        Console.WriteLine("\nDanh sách trong List:");
+        foreach (var i in phoneList)
+        {
+            i.ShowInfo();
+        }
+    }
+}
---
+++ test1110/Bai2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+
+public delegate void DiscountEventHandler(string name, int oldPrice, int newPrice);
+
+class Phone
+{
+    public string Name;
+    public int Price;
+
+    // Event
+    public event DiscountEventHandler OnDiscount;
+
+    public void ApplyDiscount(int percent)
+        {
+            int oldPrice = Price;
+            int newPrice = Price - Price * percent / 100;
+            Price = newPrice;
+            if (OnDiscount != null)
+            OnDiscount(Name, oldPrice, newPrice);
+    }
+    public virtual void ShowInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Price: {Price}");
+    }
+    
+}
+
+class Store
+{
+    private ArrayList phones = new ArrayList();
+    public void AddPhone(Phone p)
+    {
+        phones.Add(p);
+    }
+
+    public void DiscountAll(int percent)
+    {
+        foreach (Phone p in phones)
+        {
+            p.ApplyDiscount(percent);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Store store = new Store();
+        Phone p1 = new Phone { Name = "Nokia", Price = 5000 };
+        Phone p2 = new Phone { Name = "Nokia1", Price = 20000 };
+        Phone p3 = new Phone { Name = "Nokia2", Price = 15000 };
+        p1.OnDiscount += ShowDiscountMessage;
+        p2.OnDiscount += ShowDiscountMessage;
+        p3.OnDiscount += ShowDiscountMessage;
+        static void ShowDiscountMessage(string name, int oldPrice, int newPrice)
+        {
+            Console.WriteLine($"[EVENT] {name}: {oldPrice} -> {newPrice}");
+        }
+        store.AddPhone(p1);
+        store.AddPhone(p2);
+        store.AddPhone(p3);
+        store.DiscountAll(10);
+        Console.WriteLine("\nSau khi giảm giá:");
+        p1.ShowInfo();
+        p2.ShowInfo();
+        p3.ShowInfo();
+    }
+    class SmartPhone : Phone
+    {
+        public string OS;
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Name: {Name}, Price: {Price}, OS: {OS}");
+        }
+    }
+}
+
